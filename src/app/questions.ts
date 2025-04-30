9f36b79dc7340d21ec81c9111f2773790e6131df
maryjass <106860985+maryjass-dayo@users.noreply.github.com>
2025-04-30 22:21:46 +0900

    選択肢追加 (#3)

--- src/app/questions.ts
+++ src/app/questions.ts
@@ -5,8 +5,8 @@ export type Questions = {
 };
 
 export const questions: Questions = {
-  1: { index: 1, text: "サクッと遊べるゲームがいい？", yes: 7, no: 2 },
-  2: { index: 2, text: "ミステリーが好き？", yes: 3, no: 6 },
+  1: { index: 1, text: "勘は冴えてる方だ", yes: 7, no: 2 },
+  2: { index: 2, text: "散歩は好き？", yes: 3, no: 6 },
   3: { index: 3, text: "秋が一番好き？", yes: 4, no: 5 },
   4: {
     index: 4,
@@ -22,31 +22,31 @@ export const questions: Questions = {
   },
   5: {
     index: 5,
+    text: "旅行するなら海外がいい？",
+    yes: {
+      name: "パラダイスキラー",
+      url: "https://store-jp.nintendo.com/item/software/D70010000037418",
+    },
+    no: {
+      name: "都市伝説解体センター",
+      url: "https://store-jp.nintendo.com/item/software/D70010000073247",
+    },
+  },
+  6: {
+    index: 6,
     text: "長く続いてる趣味がある",
     yes: {
       name: "グノーシア",
       url: "https://store-jp.nintendo.com/item/software/D70010000027791",
     },
     no: {
-      name: "未解決事件は終わらせないといけないから",
-      url: "https://store-jp.nintendo.com/item/software/D70010000083094",
-    },
-  },
-  6: {
-    index: 6,
-    text: "休日は家でゆっくりするタイプ？",
-    yes: {
-      name: "ファミレスを享受せよ",
-      url: "https://store-jp.nintendo.com/item/software/D70010000070959",
-    },
-    no: {
-      name: "Unpacking",
-      url: "https://store-jp.nintendo.com/item/software/D70010000043204",
+      name: "OPUS 地球計画",
+      url: "https://store-jp.nintendo.com/item/software/D70010000002823",
     },
   },
   7: {
     index: 7,
-    text: "旅行好き？",
+    text: "chillしたい？",
     yes: {
       name: "Unpacking",
       url: "https://store-jp.nintendo.com/item/software/D70010000043204",
